d941a3dc28691757fc9e9e72f419d9010befad5c
yeojin <lyeo7272@gmail.com>
2023-03-13 18:01:51 +0900

    Initial commit

---
+++ src/main/java/com/example/springcloudservice/SpringcloudserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloudservice/SpringcloudserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
